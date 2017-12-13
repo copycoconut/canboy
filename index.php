067c1f56a6c6d4a2f6f9bfeb65fc683071b5b340
copyco <divshop01@gmail.com>
2017-12-13 12:47:27 +0700

    6012131247

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('BsV7EU1YvzdPMjCUJJXJpNhG6awBuw1zgIwGKNSb2OwV3Ql43DsouH9ZrSWI3/uh78rRdg/iFeaSNKAqxLlqSiAuHnMH0gGuaI7C+tnPnO4x2nj2V/fZ32RtsZ2GIlNslPM0TKKatp6JrJFd38DlEwdB04t89/1O/w1cDnyilFU=');
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'be2ed33205650821566bcfd71e079b09']);
 	$response = $bot->replyText('https://api.line.me/v2/bot/message/reply','hello!');
+	echo $response;
 
 ?>
